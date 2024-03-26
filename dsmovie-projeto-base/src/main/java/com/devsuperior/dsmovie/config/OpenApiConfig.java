08123819ecad2766efc191bfc2366230b5c89132
Diego Alves <djalves424@gmail.com>
2024-03-26 18:23:42 -0300

    Made a new configuration update to the class

--- dsmovie-projeto-base/src/main/java/com/devsuperior/dsmovie/config/OpenApiConfig.java
+++ dsmovie-projeto-base/src/main/java/com/devsuperior/dsmovie/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.devsuperior.dsmovie.config;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition
 @Configuration
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer")
 public class OpenApiConfig {
 
     @Bean
