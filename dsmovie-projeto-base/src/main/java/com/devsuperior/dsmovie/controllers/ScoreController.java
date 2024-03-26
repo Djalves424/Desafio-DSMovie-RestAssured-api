5950c89e199e42f3653bc92fe8119375bab0833c
Diego Alves <djalves424@gmail.com>
2024-03-26 18:25:32 -0300

    Updated the ScoreController class with swagger

--- dsmovie-projeto-base/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ dsmovie-projeto-base/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dsmovie.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,15 +20,28 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/scores")
+@Tag(name = "Score", description = "Controller for Score Movie")
 public class ScoreController {
-	
-	@Autowired
-	private ScoreService service;
-	
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
-	@PutMapping
-	public ResponseEntity<MovieDTO> saveScore(@Valid @RequestBody ScoreDTO dto) {
-		MovieDTO movieDTO = service.saveScore(dto);
-		return ResponseEntity.ok().body(movieDTO);
-	}
+
+    @Autowired
+    private ScoreService service;
+
+    @Operation(
+            description = "Endpoint for score",
+            summary = "Save score",
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200"),
+                    @ApiResponse(description = "Bad Request", responseCode = "400"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Not found", responseCode = "404"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422")})
+
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @PutMapping (produces = "application/json")
+    public ResponseEntity<MovieDTO> saveScore(@Valid @RequestBody ScoreDTO dto) {
+        MovieDTO movieDTO = service.saveScore(dto);
+        return ResponseEntity.ok().body(movieDTO);
+    }
 }
